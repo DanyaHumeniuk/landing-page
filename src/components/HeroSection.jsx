db945faa9c9f1903daadc0ed95402ddf79b84841
DanyaHumeniuk <dgumenuk076@gmail.com>
2025-04-10 22:15:57 -0700

    Completed Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,27 @@
-import hero from "../assets/heroPhoto.jpeg"
+import hero from "../assets/heroPhoto3.jpeg"
+
 
 const HeroSection = () => {
   return (
-    <section className="relative flex h-screen items-center justify-center">
-        <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
-            <img src={hero} className="h-full w-full object-cover"/>
+    <section className="grid place-items-center min-h-screen w-screen overflow-hidden">
+        
+        <div className="grid w-full h-full">
+            
+            <img
+            src={hero}
+            className="w-full h-full object-cover object-center col-start-1 row-start-1"
+            alt="Hero"
+            />
+
+            
+            <div className="col-start-1 row-start-1 flex flex-col items-center justify-center text-white px-4">
+                <h1 className="text-4xl md:text-6xl font-bold mb-4 lg:text-9xl border-b">Ольга Вітюк</h1>
+                <p className="text-lg md:text-xl lg:text-4xl">Тета-практик</p>
+                
+            </div>
         </div>
     </section>
+  
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,33 +4,33 @@ const Navbar = () => {
   return (
     <nav className="fixed top-8 left-1/2 w-[80%] -translate-x-1/2 
     p-4 bg-white/30 backdrop-blur 
-    border border-red-800/70
-    shadow-[0_6px_6px_rgba(0,0,0,0.5)] rounded-full">
+    border border-neutral-900
+    shadow-[0_6px_6px_rgba(0,0,0,0.5)] rounded-full lg:w-1/2">
         <ul className="flex items-center justify-center gap-16">
-  <li>
-    <a className="no-underline font-medium relative py-[5px] transition-colors duration-300 ease-in-out group">
-      Послуги
-      <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-red-700 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"></span>
-    </a>
-  </li>
-  <li>
-    <a className="no-underline  font-medium relative py-[5px] transition-colors duration-300 ease-in-out group">
-      Про мене
-      <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-red-700 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"></span>
-    </a>
-  </li>
-  <li>
-    <a className="no-underline  font-medium relative py-[5px] transition-colors duration-300 ease-in-out group ">
-      Відгуки
-      <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-red-700 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"></span>
-    </a>
-  </li>
-  <li>
-    <a className="no-underline font-medium relative py-[5px] transition-colors duration-300 ease-in-out group">
-      Контакт
-      <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-red-700 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"></span>
-    </a>
-  </li>
+            <li>
+                <a className="no-underline font-medium relative py-[5px] transition-colors duration-300 ease-in-out group cursor-pointer">
+                Послуги
+                <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-red-700 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"></span>
+                </a>
+            </li>
+            <li>
+                <a className="no-underline  font-medium relative py-[5px] transition-colors duration-300 ease-in-out group cursor-pointer">
+                Про мене
+                <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-red-700 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"></span>
+                </a>
+            </li>
+            <li>
+                <a className="no-underline  font-medium relative py-[5px] transition-colors duration-300 ease-in-out group cursor-pointer">
+                Відгуки
+                <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-red-700 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"></span>
+                </a>
+            </li>
+            <li>
+                <a className="no-underline font-medium relative py-[5px] transition-colors duration-300 ease-in-out group cursor-pointer">
+                Контакт
+                <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-red-700 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"></span>
+                </a>
+            </li>
 </ul>
     </nav>
   )
